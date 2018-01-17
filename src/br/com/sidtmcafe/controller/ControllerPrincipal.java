41a83d02850ee94f7838f6790aa4907357b9cff3
thiagomacedo <tl.macedo@hotmail.com>
2018-01-16 21:20:32 -0400

    auto-commit

--- src/br/com/sidtmcafe/controller/ControllerPrincipal.java
+++ src/br/com/sidtmcafe/controller/ControllerPrincipal.java
@@ -5,8 +5,7 @@ import br.com.sidtmcafe.model.dao.SisMenuPrincipalDAO;
 import br.com.sidtmcafe.model.vo.SisMenuPrincipalVO;
 import com.jfoenix.controls.JFXToolbar;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -17,10 +16,15 @@ import java.util.ResourceBundle;
 
 public class ControllerPrincipal implements Initializable, FormularioModelo {
 
+
     public BorderPane pnlViewPrincipal;
     public JFXToolbar stbViewPrincipal;
-    public ImageView btnRetraiMenuViewPrincipal;
-    public TreeView treeMenuViewPrincipal;
+    public Label btnRetraiMenuViewPrincipal;
+    public Label btnExpandeMenuViewPrincipal;
+    public TreeTableView treeMenuViewPrincipal;
+
+    TreeTableColumn<SisMenuPrincipalVO, String> colunaItem;
+    TreeTableColumn<SisMenuPrincipalVO, String> colunaAtalho;
 
     @Override
     public void fechar(Stage stage) {
@@ -29,25 +33,7 @@ public class ControllerPrincipal implements Initializable, FormularioModelo {
 
     @Override
     public void preencherObjetos() {
-        List<SisMenuPrincipalVO> menuPrincipalVOList = new SisMenuPrincipalDAO().getMenuPrincipalVOList();
-        TreeItem[] treeItems = new TreeItem[menuPrincipalVOList.size() + 1];
-        treeItems[0] = new TreeItem();
-
-        for (SisMenuPrincipalVO principalVO : menuPrincipalVOList) {
-            int idTemp = principalVO.getId();
-            int filhoTemp = principalVO.getFilho_id();
-            String icoMenu = principalVO.getIcoMenu();
-            if (icoMenu.equals("")) {
-                treeItems[idTemp] = new TreeItem(principalVO);
-            } else {
-                treeItems[idTemp] = new TreeItem(principalVO, new ImageView("/images/material-design-icons" + icoMenu));
-            }
-            treeItems[idTemp].setExpanded(true);
-            treeItems[filhoTemp].getChildren().add(treeItems[idTemp]);
-        }
-
-        treeMenuViewPrincipal.setRoot(treeItems[0]);
-        treeMenuViewPrincipal.setShowRoot(false);
+        preencheMenuItem();
     }
 
     @Override
@@ -63,5 +49,43 @@ public class ControllerPrincipal implements Initializable, FormularioModelo {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         preencherObjetos();
+    }
+
+    void preencheMenuItem() {
+        /*colunaRazao = new JFXTreeTableColumn<EmpresasVO, String>();
+        colunaRazao.setPrefWidth(200);
+        Label labelRazao = new Label("Razão / Nome");
+        labelRazao.setPrefWidth(200);
+        colunaRazao.setGraphic(labelRazao);
+        colunaRazao.setCellValueFactory(param -> param.getValue().getValue().razaoProperty());*/
+
+        colunaItem = new TreeTableColumn<SisMenuPrincipalVO, String>();
+        colunaItem.setPrefWidth(250);
+        colunaItem.setCellValueFactory(param -> param.getValue().getValue().descricaoProperty());
+
+        colunaAtalho = new TreeTableColumn<SisMenuPrincipalVO, String>();
+        colunaAtalho.setPrefWidth(150);
+        colunaAtalho.setCellValueFactory(param -> param.getValue().getValue().teclaAtalhoProperty());
+
+        List<SisMenuPrincipalVO> menuPrincipalVOList = new SisMenuPrincipalDAO().getMenuPrincipalVOList();
+        TreeItem[] treeItems = new TreeItem[menuPrincipalVOList.size() + 1];
+        treeItems[0] = new TreeItem();
+
+        for (SisMenuPrincipalVO principalVO : menuPrincipalVOList) {
+            int idTemp = principalVO.getId();
+            int filhoTemp = principalVO.getFilho_id();
+            String icoMenu = principalVO.getIcoMenu();
+            if (icoMenu.equals("")) {
+                treeItems[idTemp] = new TreeItem(principalVO);
+            } else {
+                treeItems[idTemp] = new TreeItem(principalVO, new ImageView("/images/material-design-icons/" + icoMenu));
+            }
+            treeItems[idTemp].setExpanded(true);
+            treeItems[filhoTemp].getChildren().add(treeItems[idTemp]);
+        }
+
+        treeMenuViewPrincipal.getColumns().setAll(colunaItem, colunaAtalho);
+        treeMenuViewPrincipal.setRoot(treeItems[0]);
+        treeMenuViewPrincipal.setShowRoot(false);
     }
 }
